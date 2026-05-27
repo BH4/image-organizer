ccb7f51e9cce1ec8bec2d96ac67e90064ce3f908
BH4 <BH4@users.noreply.github.com>
2026-05-26 23:47:14 -0500

    Improve find_images.py and display.py
    Fixed bug for pngs in find_images.py and decreased frequency of currnet count print.
    
    Display now has a function to show up to 8 images at a time.

--- bin/display.py
+++ bin/display.py
@@ -1,8 +1,53 @@
+from PIL import Image
+import matplotlib
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 
 
 def show_image(filename):
-    img = mpimg.imread(filename)
-    plt.imshow(img)
+    image = Image.open(filename)
+    image.show()
+
+
+def text_wrap(text, width=20):
+    new_text = ''
+    for ind, c in enumerate(text):
+        new_text += c
+        if (ind+1) % width == 0:
+            new_text += '\n'
+    return new_text
+
+
+def show_image_list(filenames, figsize_per_image=(4, 4)):
+    n = len(filenames)
+    if n > 8:
+        print('Too many to display at once.')
+        return
+
+    num_rows = ((n-1) // 4) + 1
+    num_cols = min(n, 4)
+
+    font = {'size': 6}
+    matplotlib.rc('font', **font)
+
+    fig, axes = plt.subplots(
+        num_rows,
+        num_cols,
+        figsize=(figsize_per_image[0] * num_cols, figsize_per_image[1] * num_rows),
+    )
+
+    if n == 1:
+        axes = [axes]
+    else:
+        axes = axes.ravel()
+
+    for ax, fname in zip(axes, filenames):
+        img = Image.open(fname)
+        ax.imshow(img)
+        ax.set_title(text_wrap(fname, width=35))
+        ax.axis("off")
+
+    for ax in axes[n:]:
+        ax.axis("off")
+
+    plt.tight_layout()
     plt.show()
--- bin/find_images.py
+++ bin/find_images.py
@@ -1,8 +1,7 @@
 import os
 
 
-picture_extensions = set(['jpg', 'jpeg', '.png'])
-BLOCKSIZE = 65536
+picture_extensions = set(['jpg', 'jpeg', 'png'])
 
 
 def image_finder(start_folder):
@@ -10,8 +9,7 @@ def image_finder(start_folder):
     Finds all images in the starting folder and recursively searches lower
     folders.
 
-    Returns dictionary of arrays of filenames. Duplicate images will have
-    there filenames in the same array.
+    Returns list of filenames.
 
     Picture extensions included: .jpg, .png, .jpeg
     """
@@ -24,7 +22,7 @@ def image_finder(start_folder):
             ext = name.split('.')[-1].lower()
             if ext in picture_extensions:
                 num_checked += 1
-                if num_checked % 100 == 0:
+                if num_checked % 1000 == 0:
                     print(num_checked)
 
                 file_path = os.path.join(root, name)
